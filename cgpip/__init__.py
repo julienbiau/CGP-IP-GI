b35eeea5170d667c818867a3e45542858c2bdd3c
julienbiau <julien.biau@gmail.com>
2022-06-03 17:13:59 +0200

    __init__.py added

---
+++ cgpip/__init__.py
@@ -0,0 +1,7 @@
+from .cgpip import CGPIP
+from .island import Island
+from .chromosome import Chromosome
+from .functions import Functions
+from .fct_std_uint8 import STD_UINT8
+from .fct_ext_uint8 import EXT_UINT8
+from .fct_std_uint8_mp import STD_UINT8_MP
